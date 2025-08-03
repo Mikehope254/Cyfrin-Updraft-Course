34a8d64ca43c2aa6c78bb24cb0adc1d3a3b9259b
Mikehope254 <mikehope1738@gmail.com>
2025-08-03 20:35:43 +0300

    Finshing the Setup

---
+++ Foundry Fundamentals/foundry-f23/foundry-fund-me-f25/src/FundMe.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+   
+    uint256 public constant  MINIMUM_USD = 5e18;
+    //670,925 gas - non-constant 
+    //650,627 gas - constant
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public immutable i_owner;
+    //439 immutable 
+    //2574 non-immutable
+    
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable{
+        require(msg.value.getConversionRate()>= MINIMUM_USD, "didn't send enough ");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+    
+     function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function withdraw() public onlyOwner {
+        /* starting index (initialization), ending index, step amount */
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++ ){
+        address funder = funders[funderIndex];
+        addressToAmountFunded[funder] = 0;}
+        
+        //resetting the array
+        funders = new address[](0);
+
+        //transfer
+        //payable(msg.sender) = payable address
+        //payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        //bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        //require(sendSuccess, "Send failed");
+
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value:address(this).balance}("") ;   
+        require(callSuccess, "Call Failed");
+    }
+    
+    modifier onlyOwner(){
+    	//require(msg.sender == i_owner, "Sender is not owner");
+        if(msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+    	_;
+    }
+       
+       receive() external payable {
+        fund();
+    }
+    
+    fallback() external payable {
+        fund();
+    }
+}
