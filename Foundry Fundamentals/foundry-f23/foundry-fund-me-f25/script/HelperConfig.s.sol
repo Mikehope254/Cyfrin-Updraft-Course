7b9e71b3c6d06e2d0b91867d8fe5ffef1802b016
Mikehope254 <mikehope1738@gmail.com>
2025-08-10 18:31:15 +0300

    script folder updated

--- Foundry Fundamentals/foundry-f23/foundry-fund-me-f25/script/HelperConfig.s.sol
+++ Foundry Fundamentals/foundry-f23/foundry-fund-me-f25/script/HelperConfig.s.sol
@@ -11,9 +11,36 @@ contract HelperConfig {
     // if we are on a local anvil, we deploy mocks
     // Otherwise, grab the existing address from the live network
 
-    function getSepoliaEthConfig() public pure {
+    NetworkConfig public activeNetworkConfig;
 
+    struct NetworkConfig {
+        address priceFeed;  //ETH/USD PriceFeed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        }
+        else if(block.chainid == 1){
+            activeNetworkConfig = getMainnetEthConfig();
+        } 
+        else {
+            activeNetworkConfig = getAnvilConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory){
+        //price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaConfig;
     } 
 
-    function getAnvil
+function getMainnetEthConfig() public pure returns (NetworkConfig memory){
+        //price feed address
+        NetworkConfig memory ethConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
+        return ethConfig;
+    }
+    function getAnvilConfig() public pure returns (NetworkConfig memory){
+        //price feed address
+    }
 }
